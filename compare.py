c2c3f2574cf9bb261c6be2bac87f053dcbdba242
Sanchay Devnath <sanchaydevnath@gmail.com>
2026-07-14 10:23:07 -0400

    Curvature Function Reiterated

--- compare.py
+++ compare.py
@@ -6,7 +6,7 @@ class ManifoldComparator:
         ID_diff = abs(m1.get_intrinsic_dim() - m2.get_intrinsic_dim())
         maxCurve = max(m1.compute_ollivier_ricci()['mean_curvature'], m2.compute_ollivier_ricci()['mean_curvature'])
         minCurve = min(m1.compute_ollivier_ricci()['mean_curvature'], m2.compute_ollivier_ricci()['mean_curvature'])
-        curv_diff = (maxCurve - minCurve) / maxCurve if maxCurve != 0 else 0
+        curv_diff = (maxCurve - minCurve) / 3 if maxCurve != 0 else 0 #Normalize by curvatures range[1,-2]
 
         return ID_diff, curv_diff
 
